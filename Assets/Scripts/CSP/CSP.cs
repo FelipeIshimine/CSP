45c558b39e11c53968268661e285a410a54523dd
Felipe Ishimine <felipeishimine@gmail.com>
2021-06-07 08:50:53 -0300

    NQueen Ready

--- Assets/Scripts/CSP/CSP.cs
+++ Assets/Scripts/CSP/CSP.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 
 public class CSP<V,D>
 {
-    private Dictionary<V, List<D>> _domain;
+    private readonly Dictionary<V, List<D>> _domain;
     private readonly V[] _variables;
     private readonly Dictionary<V, List<Constraint<V,D>>> _constraint; 
  
@@ -15,6 +16,8 @@ public class CSP<V,D>
         _domain = domain;
         _constraint = new Dictionary<V, List<Constraint<V,D>>>();
         _variables = variables;
+        foreach (V variable in _variables)
+            _constraint.Add(variable,new List<Constraint<V, D>>());
     }
 
     public void AddConstraint(Constraint<V, D> nConstraint)
@@ -57,9 +60,7 @@ public class CSP<V,D>
         return unassigned;
     }
 
-    
-    
-    public Dictionary<V, D> BacktrackingSearch(Dictionary<V, D> assignment)
+    public Dictionary<V, D> BacktrackingSearchRecursive(Dictionary<V, D> assignment)
     {
         if (assignment.Count == _variables.Length)
             return assignment; //Asignacion de todas las variables
@@ -67,43 +68,187 @@ public class CSP<V,D>
         var unassigned = CollectUnassigned(assignment);
         V first = unassigned[0];
 
+        foreach (D value in _domain[first])
+        {
+            var localAssignment = new Dictionary<V, D>(assignment);
+            localAssignment[first] = value;
+
+            if (Consistent(first, localAssignment))
+            {
+                var result = BacktrackingSearchRecursive(localAssignment);
+                if (result != null)
+                    return result;
+            }
+        }
+        return null;
+    }
+
+    public void AllBacktrackingSearchRecursive(Dictionary<V, D> assignment, ref List<Dictionary<V, D>> solutions)
+    {
+        if (assignment.Count == _variables.Length)//Asignacion de todas las variables
+        {
+            solutions.Add(assignment);
+            return; 
+        }
+
+        var unassigned = CollectUnassigned(assignment);
+        V first = unassigned[0];
 
         foreach (D value in _domain[first])
         {
             var localAssignment = new Dictionary<V, D>(assignment);
             localAssignment[first] = value;
 
-            if (!Consistent(first, localAssignment)) continue;
-            
-            var result = BacktrackingSearch(localAssignment);
-            if (result != null)
-                return result;
+            if (Consistent(first, localAssignment))
+                AllBacktrackingSearchRecursive(localAssignment, ref solutions);
         }
-        return null;
+        return;
     }
-  /*
+   
 
-    def backtracking_search(self, assignment: Dict[V, D] = {}) -> Optional[Dict[V, D]]:
-# assignment is complete if every variable is assigned (our base case)
-    if len(assignment) == len(self.variables):
-        return assignment
+    public List<Dictionary<V, D>> BacktrackingSearchAllIterative(int maxSolutionCount = int.MaxValue)
+    {
+        Dictionary<V, D> assignment = new Dictionary<V, D>();
+        List<Dictionary<V, D>> solutions = new List<Dictionary<V, D>>();
+        var list = new List<V>(_variables);
+        list.Reverse();
+        Stack<V> next = new Stack<V>(list);
+        Stack<V> previous = new Stack<V>();
 
-# get all variables in the CSP but not in the assignment
-        unassigned: List[V] = [v for v in self.variables if v not in assignment]
+        while (next.Count > 0) //Mientras hay valores sin asignar seguimos
+        {
+            V currentVariable = next.Pop(); //Sacamos el siguiente valor sin asignacion correcta
+            previous.Push(currentVariable);
+            int valueIndex = 0;
 
-# get the every possible domain value of the first unassigned variable
-    first: V = unassigned[0]
-    for value in self.domains[first]:
-        local_assignment = assignment.copy()
-        local_assignment[first] = value
-        # if we're still consistent, we recurse (continue)
-        if self.consistent(first, local_assignment):
-            result: Optional[Dict[V, D]] = self.backtracking_search(local_assignment)
-            # if we didn't find the result, we will end up backtracking
-            if result is not None:
-                return result
-    return None
-    */
+            if (assignment.ContainsKey(currentVariable)) //Si el valor fue asignado significa que su valor fue asignado pero incorrecto (no llego a una solucion)
+                valueIndex = _domain[currentVariable].IndexOf(assignment[currentVariable])+1;
+
+            if (valueIndex < _domain.Count) //Si el index es mayor al numero de posibles valores en el dominio, significa que este camino no es "consistente" y se debe hacer un backtracking
+            {
+
+                assignment[currentVariable] = _domain[currentVariable][valueIndex];
+                if (!Consistent(currentVariable, assignment))
+                    next.Push(previous.Pop());
+                else if (assignment.Count == _variables.Length)
+                {
+                    solutions.Add(new Dictionary<V, D>(assignment));
+
+                    if(solutions.Count != maxSolutionCount) //Si todavia no completamos el numero de soluciones necesarias, seguimos como si no hubieramos encontrado nada
+                        next.Push(previous.Pop());
+                }
+            }
+            else
+            {
+                next.Push(previous.Pop());
+                if (previous.Count == 0)
+                    break; //Ya no hay mas posibles combinaciones
+
+                //Agregamos la variable previa a la pila y eliminamos el valor de la variable actual, para iterar nuevamente desde 0
+                assignment.Remove(currentVariable);
+                next.Push(previous.Pop());
+            }
+        }
+
+        return solutions;
+    }
+
+    public List<Dictionary<V, D>> RandomSolutions(int maxSolutionCount = int.MaxValue)
+    {
+        Dictionary<V, D> assignment = new Dictionary<V, D>();
+        List<Dictionary<V, D>> solutions = new List<Dictionary<V, D>>();
+        var list = new List<V>(_variables);
+        list.Reverse();
+        Stack<V> next = new Stack<V>(list);
+        Stack<V> previous = new Stack<V>();
+
+        Dictionary<V, Queue<D>> activeTempDomain = new Dictionary<V, Queue<D>>();
+
+        Dictionary<V, List<D>> activeRandomOrder = new Dictionary<V, List<D>>();
+
+        foreach (var cVariable in _variables)
+        {
+            Queue<D> possibleValues = new Queue<D>();
+            var cVariableDomain = new List<D>(_domain[cVariable]);
+            int count = cVariableDomain.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                int index = UnityEngine.Random.Range(0, cVariableDomain.Count);
+                var value = cVariableDomain[index];
+                possibleValues.Enqueue(value);
+                cVariableDomain.RemoveAt(index);
+            }
+            activeRandomOrder.Add(cVariable, new List<D>(possibleValues));
+        }
+
+
+        while (next.Count > 0) //Mientras hay valores sin asignar seguimos
+        {
+            V currentVariable = next.Pop(); //Sacamos el siguiente valor sin asignacion correcta
+            previous.Push(currentVariable);
+
+            if (!activeTempDomain.ContainsKey(currentVariable))
+                activeTempDomain.Add(currentVariable, new Queue<D>(activeRandomOrder[currentVariable]));
+
+            var valuesQueue = activeTempDomain[currentVariable];
+
+            if (valuesQueue.Count > 0)
+            {
+                assignment[currentVariable] = valuesQueue.Dequeue();
+
+                if (!Consistent(currentVariable, assignment))
+                {
+                    next.Push(previous.Pop());
+                }
+                else if (assignment.Count == _variables.Length)
+                {
+                    solutions.Add(new Dictionary<V, D>(assignment));
+                    if (solutions.Count != maxSolutionCount) //Si todavia no completamos el numero de soluciones necesarias, seguimos como si no hubieramos encontrado nada
+                        next.Push(previous.Pop());
+                }
+            }
+            else
+            {
+                assignment.Remove(currentVariable);
+                activeTempDomain.Remove(currentVariable);
+                next.Push(previous.Pop());
+                if (previous.Count == 0)
+                    break; //Ya no hay mas posibles combinaciones
+
+                var previousValue = previous.Pop();
+                next.Push(previousValue);
+            }
+        }
+
+        return solutions;
+    }
+
+
+
+    /*
+
+      def backtracking_search(self, assignment: Dict[V, D] = {}) -> Optional[Dict[V, D]]:
+  # assignment is complete if every variable is assigned (our base case)
+      if len(assignment) == len(self.variables):
+          return assignment
+
+  # get all variables in the CSP but not in the assignment
+          unassigned: List[V] = [v for v in self.variables if v not in assignment]
+
+  # get the every possible domain value of the first unassigned variable
+      first: V = unassigned[0]
+      for value in self.domains[first]:
+          local_assignment = assignment.copy()
+          local_assignment[first] = value
+          # if we're still consistent, we recurse (continue)
+          if self.consistent(first, local_assignment):
+              result: Optional[Dict[V, D]] = self.backtracking_search(local_assignment)
+              # if we didn't find the result, we will end up backtracking
+              if result is not None:
+                  return result
+      return None
+      */
 }
 
 public abstract class Constraint<T, B>
@@ -139,19 +284,35 @@ public class QueenConstraint : Constraint<int, int>
     {
         foreach (KeyValuePair<int,int> pair in assignment)
         {
+            //Debug.Log($"Assignment: [{pair.Key}]=={pair.Value}");
             int currentColumn = pair.Key;
             int currentRow = pair.Value;
 
-            for (int i = currentColumn; i < Variables.Length; i++)
+            for (int i = currentColumn+1; i < Variables.Length+1; i++)
             {
-                if(!assignment.ContainsKey(i)) continue;
+             
+                if (!assignment.ContainsKey(i))
+                    continue;
+
                 int targetColumn = i;
                 int targetRow = assignment[i];
+                //Debug.Log($"[{currentColumn},{currentRow}] [{targetColumn},{targetRow}]");
 
-                if (currentColumn == targetColumn) return false;//misma columna
-                if(Mathf.Abs(currentRow - targetRow) == Mathf.Abs(currentColumn-targetColumn))  return false;//Misma diagonal
+                if (currentRow == targetRow)
+                {
+                    //Debug.Log("Assignment:False SAME ROW}");
+                    return false;//Same row
+                }
+
+//                Debug.Log($"{Mathf.Abs(currentColumn - targetColumn)}=={Mathf.Abs(currentRow - targetRow)}");
+                if (Mathf.Abs(currentColumn - targetColumn) == Mathf.Abs(currentRow - targetRow))
+                {
+                    //Debug.Log("Assignment:False     SAME DIAGONAL}");
+                    return false;//Misma diagonal
+                }
             }
         }
+        //Debug.Log("Assignment:True}");
         return true;
     }
 }
--- Assets/Scripts/CSP/NQueenTest.cs
+++ Assets/Scripts/CSP/NQueenTest.cs
@@ -1,42 +1,208 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class NQueenTest : MonoBehaviour
 {
     [Button]
-    public void Execute()
+    public void FirstSolutionComparison(int n = 8)
     {
+        var stopWatch = new Stopwatch();
+        stopWatch.Start();
+        ExecuteRecursive(n, false);
+        stopWatch.Stop();
+        var recursive = stopWatch.Elapsed;
 
-        var columns = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
+        stopWatch = new Stopwatch();
+        stopWatch.Start();
+        ExecuteAllIterative(n, 1, false);
+        stopWatch.Stop();
+
+        var iterative = stopWatch.Elapsed;
+
+        stopWatch = new Stopwatch();
+        stopWatch.Start();
+        ExecuteAllIterativeRandomSolution(n, 1, false);
+        stopWatch.Stop();
+
+        var iterativeRandom = stopWatch.Elapsed;
+
+        Debug.Log($"Recursive:{recursive.TotalMilliseconds}   |   IterativeLineal:{iterative.TotalMilliseconds} | IterativeRandom:{iterativeRandom.TotalMilliseconds}");
+    }
+
+    [Button]
+    public void RandomSolutionMean(int n = 8, int repetitions = 100)
+    {
+        double totalTime = 0;
+
+        for (int i = 0; i < repetitions; i++)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            ExecuteAllIterativeRandomSolution(n, 1, false);
+            stopWatch.Stop();
+            totalTime += stopWatch.Elapsed.TotalMilliseconds;
+
+        }
+        Debug.Log($"totalTime:{totalTime}");
+        Debug.Log($"Mean:{totalTime / repetitions} Milliseconds");
+    }
+
+
+    [Button]
+    public void AllSolutionsComparison(int n = 8)
+    {
+        var stopWatch = new Stopwatch();
+        stopWatch.Start();
+        ExecuteAllRecursive(n, false);
+        stopWatch.Stop();
+
+        var recursive = stopWatch.Elapsed;
+
+        stopWatch = new Stopwatch();
+        stopWatch.Start();
+        ExecuteAllIterative(n, int.MaxValue, false);
+        stopWatch.Stop();
+
+        var iterative = stopWatch.Elapsed;
+
+        stopWatch = new Stopwatch();
+        stopWatch.Start();
+        ExecuteAllIterativeRandomSolution(n, int.MaxValue, false);
+        stopWatch.Stop();
+
+        var iterativeRandom = stopWatch.Elapsed;
+
+        Debug.Log($"Recursive:{recursive}   |   IterativeLineal:{iterative} | IterativeRandom:{iterativeRandom}");
+
+    }
+
+    [Button]
+    public void ExecuteRecursive(int n = 8, bool printSolution = true)
+    {
+        var columns = new int[n];
+        for (int i = 0; i < n; i++)
+            columns[i] = i + 1;
+
         var rows = new Dictionary<int, List<int>>();
 
         foreach (int column in columns)
-            rows[column] = new List<int>() {1, 2, 3, 4, 5, 6, 7, 8};
+            rows[column] = new List<int>(columns);
 
         var csp = new CSP<int, int>(columns, rows);
-
         csp.AddConstraint(new QueenConstraint(columns));
 
-        Dictionary<int, int> solution = csp.BacktrackingSearch(new Dictionary<int, int>());
-        
-        if(solution == null)
-            Debug.Log("No Solution found");
+        Dictionary<int, int> solution = csp.BacktrackingSearchRecursive(new Dictionary<int, int>());
+
+        if (!printSolution)
+            return;
+        if (solution == null)
+            UnityEngine.Debug.Log("No Solution found");
         else
-            Debug.Log(solution.ToString());
+            SolutionToString(solution);
+    }
+
+    [Button]
+    public void ExecuteAllRecursive(int n=8, bool printSolution = true)
+    {
+        var columns = new int[n];
+        for (int i = 0; i < n; i++)
+            columns[i] = i + 1;
+
+        var rows = new Dictionary<int, List<int>>();
+
+        foreach (int column in columns)
+            rows[column] = new List<int>(columns);
+
+        var csp = new CSP<int, int>(columns, rows);
+        csp.AddConstraint(new QueenConstraint(columns));
+        List<Dictionary<int, int>> solutions = new List<Dictionary<int, int>>();
+        csp.AllBacktrackingSearchRecursive(new Dictionary<int, int>(), ref solutions);
+
+        if (!printSolution) return;
+
+        if(solutions.Count == 0)
+            UnityEngine.Debug.Log("No Solution found");
+        else
+        {
+            foreach (var solution in solutions)
+                SolutionToString(solution);
+        }
+    }
+
+    [Button]
+    public void ExecuteAllIterative(int n = 8, int maxSolutionCount = int.MaxValue, bool printSolution = true)
+    {
+        var columns = new int[n];
+        for (int i = 0; i < n; i++)
+            columns[i] = i + 1;
+
+        var rows = new Dictionary<int, List<int>>();
+
+        foreach (int column in columns)
+            rows[column] = new List<int>(columns);
+
+        var csp = new CSP<int, int>(columns, rows);
+        csp.AddConstraint(new QueenConstraint(columns));
+        List<Dictionary<int, int>> solutions = csp.BacktrackingSearchAllIterative(maxSolutionCount);
+
+        if (!printSolution) return;
+
+        if (solutions.Count == 0)
+            UnityEngine.Debug.Log("No Solution found");
+        else
+        {
+            foreach (var solution in solutions)
+                SolutionToString(solution);
+        }
+    }
+
+    [Button]
+    public void ExecuteAllIterativeRandomSolution(int n = 8, int maxSolutionCount = int.MaxValue, bool printSolution = true)
+    {
+        var columns = new int[n];
+        for (int i = 0; i < n; i++)
+            columns[i] = i + 1;
+
+        var rows = new Dictionary<int, List<int>>();
+
+        foreach (int column in columns)
+            rows[column] = new List<int>(columns);
+
+        var csp = new CSP<int, int>(columns, rows);
+        csp.AddConstraint(new QueenConstraint(columns));
+        List<Dictionary<int, int>> solutions = csp.RandomSolutions(maxSolutionCount);
+
+        if (!printSolution) return;
+
+        if (solutions.Count == 0)
+            UnityEngine.Debug.Log("No Solution found");
+        else
+        {
+            foreach (var solution in solutions)
+                SolutionToString(solution);
+        }
+    }
+
+    private void SolutionToString(Dictionary<int, int> solution)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        foreach (var item in solution)
+        {
+            for (int i = 1; i < solution.Keys.Count+1; i++)
+            {
+                if(item.Value==i)
+                    stringBuilder.Append("Q");
+                else
+                    stringBuilder.Append("#");
+            }
+            stringBuilder.Append("\n");
+        }
+        UnityEngine.Debug.Log(stringBuilder);
     }
 }
-
-
-if __name__ == "__main__":
-    columns: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
-    rows: Dict[int, List[int]] = {}
-    for column in columns:
-        rows[column] = [1, 2, 3, 4, 5, 6, 7, 8]
-    csp: CSP[int, int] = CSP(columns, rows)
-    csp.add_constraint(QueensConstraint(columns))
-    solution: Optional[Dict[int, int]] = csp.backtracking_search()
-    if solution is None:
-        print("No solution found!")
-    else:
-        print(solution)
